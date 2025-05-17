4f5c3ababd5ab3357eac2ddbda56ee0e945e4b30
ttqteo <tuquangtran123@gmail.com>
2025-05-17 16:29:07 +0700

    feat: change gold price data ui

--- app/(site)/(public)/page.tsx
+++ app/(site)/(public)/page.tsx
@@ -1,5 +1,5 @@
 import { FinancialNews } from "@/components/homepage/financial-news";
-import GoldTable from "@/components/homepage/gold-table";
+import { GoldPriceDataTable } from "@/components/homepage/gold-price-table";
 import { MarketHeatmap } from "@/components/homepage/market-heatmap";
 import { MarketIndexes } from "@/components/homepage/market-indexes";
 import {
@@ -9,14 +9,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import { getAllBlogs } from "@/lib/markdown";
 import { stringToDate } from "@/lib/utils";
 import { commodity } from "@/lib/vnstock";
@@ -28,14 +20,51 @@ import {
 } from "lucide-react";
 import { IGoldPriceV2 } from "vnstock-js";
 
+const WORLD_TYPE = ["XAUUSD", "USDX"];
+
 export default async function MarketDashboardPage() {
   const blogs = (await getAllBlogs()).sort(
     (a, b) => stringToDate(b.date).getTime() - stringToDate(a.date).getTime()
   );
   const goldPrice = (await commodity.goldPriceV2()) as IGoldPriceV2[];
+  const VietNamGold = goldPrice.filter(
+    (_) => !WORLD_TYPE.includes(_.type_code)
+  );
+  const WorldGold = goldPrice.filter((_) => WORLD_TYPE.includes(_.type_code));
 
   return (
     <>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+        <Card className="col-span-4">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <div>
+              <CardTitle className="text-base font-medium">
+                Giá Vàng Trong Nước
+              </CardTitle>
+              <CardDescription>Nguồn giavang.net</CardDescription>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <GoldPriceDataTable goldPrice={VietNamGold} />
+          </CardContent>
+        </Card>
+
+        <Card className="col-span-3">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <div>
+              <CardTitle className="text-base font-medium">
+                Financial News
+              </CardTitle>
+              <CardDescription>Latest market updates and news</CardDescription>
+            </div>
+            <Newspaper className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <FinancialNews blogs={blogs} />
+          </CardContent>
+        </Card>
+      </div>
+
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
         <Card className="col-span-4">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
@@ -58,49 +87,19 @@ export default async function MarketDashboardPage() {
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <div>
               <CardTitle className="text-base font-medium">
-                Financial News
+                Market Heatmap & Liquidity
               </CardTitle>
-              <CardDescription>Latest market updates and news</CardDescription>
+              <CardDescription>
+                Sector performance and trading volume
+              </CardDescription>
             </div>
-            <Newspaper className="h-4 w-4 text-muted-foreground" />
+            <BarChart3 className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
-            <FinancialNews blogs={blogs} />
+            <MarketHeatmap />
           </CardContent>
         </Card>
       </div>
-
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <div>
-            <CardTitle className="text-base font-medium">
-              Market Heatmap & Liquidity
-            </CardTitle>
-            <CardDescription>
-              Sector performance and trading volume
-            </CardDescription>
-          </div>
-          <BarChart3 className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <MarketHeatmap />
-        </CardContent>
-      </Card>
-
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <div>
-            <CardTitle className="text-base font-medium">
-              Giá Vàng Trong Nước
-            </CardTitle>
-            <CardDescription>Nguồn giavang.net</CardDescription>
-          </div>
-          <CircleDollarSignIcon className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <GoldTable data={goldPrice} />
-        </CardContent>
-      </Card>
     </>
   );
 }
